c04674142f406ee2329e586a94cc442a9d80286f
SaifilMubarok <saifilmubarok23@gmail.com>
2025-05-28 09:15:28 +0700

    first commit

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,23 @@
 <?php
 session_start();
-if(isset($_SESSION["username"])){
-    echo "<h1>Selamat datang, " . $_SESSION["username"];
-}else{
-    header("location:login.php");
+if (!isset($_SESSION['username'])) {
+    // User is already logged in, redirect to welcome page  
+    header("Location: login.php");
 }
+?>
+<html>
+    <head>
+        <title>::Login Page::</title>
+        <style type="text/css">
+            body{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+            }
+        </style>
+    </head>
+    <body>
+        <h1><?php echo "Selamat datang " . $_SESSION['username']; ?></h1>
+    </body>
+</html>
--- login.php
+++ login.php
@@ -1,41 +1,34 @@
 <?php
 session_start();
-if(isset($_POST["username"]) && isset($_POST["Password"])){
-    $username = $_POST["username"];
-    $Password = $_POST["Password"];
-
-    if($username == "usm" && $Password == "123"){
-        $_SESSION["username"] = "usm";
-        header("Location:dasboard.php");
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    // User is already logged in, redirect to welcome page  
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if ($username == "usm" && $password == "123") {
+        // Set session variable
+        $_SESSION['username'] = $username;
+        header("Location: dashboard.php");
     }
 }
-
-
-
 ?>
-
-
 <html>
     <head>
-        <title>login page</title>
+        <title>::Login Page::</title>
         <style type="text/css">
             body{
-                font-family: Arial, sans-serif;
-      background-image: url('https://tse2.mm.bing.net/th?id=OIP.LmihmVjmDEpr1SVThLqixQHaEK&pid=Api&P=0&h=180');
-      background-size: cover;
-      background-position: center;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      margin: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background-size: cover;
+                background-image: url("https://cdn.arstechnica.net/wp-content/uploads/2023/06/bliss-update-1440x960.jpg");
             }
             table{
                 background-color: white;
-                border: 3px solid gray;
+                border: 3px solid grey;
                 padding: 20px;
                 border-radius: 10px;
-                font-family: Arial, Helvetica, sans-serif;
+                font-family:Arial, Helvetica, sans-serif;
             }
             td{
                 padding: 5px;
@@ -48,18 +41,18 @@ if(isset($_POST["username"]) && isset($_POST["Password"])){
         </style>
     </head>
     <body>
-        <form method="post" action="login.php">
-        <table>
+        <form action="login.php" method="post">
+         <table>
             <tr>
-                <td colspan="2" style="text-align: center;">LOGIN</td>
+                <td colspan="2" style="text-align: center;" >LOGIN</td>
             </tr>
             <tr>
-                <td>username</td>
+                <td>Username</td>
                 <td><input type="text" name="username" /></td>
             </tr>
             <tr>
                 <td>Password</td>
-                <td><input type="text" name="Password"/><td>
+                <td><input type="password" name="password" /></td>
             </tr>
             <tr>
                 <td colspan="2"><input type="checkbox" /> Ingatkan saya</td>
@@ -68,6 +61,6 @@ if(isset($_POST["username"]) && isset($_POST["Password"])){
                 <td colspan="2" style="text-align: center;"><button type="submit" >SUBMIT</button></td>
             </tr>
         </table>
-    </form>
+        </form>
     </body>
 </html>
