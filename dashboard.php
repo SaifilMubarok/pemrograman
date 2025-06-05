3a39b2373d0506fc30ecb6dec7383ebeeac7f1d4
SaifilMubarok <saifilmubarok23@gmail.com>
2025-06-05 15:31:47 +0700

    tambahan pratek

--- dashboard.php
+++ dashboard.php
@@ -26,6 +26,19 @@ $count++;
 // Simpan kembali ke file
 file_put_contents($file, $count);
 
+if(!isset($_SESSION["daftar"])){
+    $_SESSION["daftar"] = [];
+
+}
+if(isset($_POST["nama"]) && isset($_POST["umur"])){
+    $daftar = [
+        "nama"=> $_POST["nama"],
+        "umur"=> $_POST["umur"]
+    ];
+
+    $_SESSION["daftar"][]=$daftar;
+}
+
 ?>
 <html>
     <head>
@@ -33,6 +46,7 @@ file_put_contents($file, $count);
         <style type="text/css">
             body{
                 display: flex;
+                flex-direction: column;
                 justify-content: center;
                 align-items: center;
                 height: 100vh;
@@ -40,9 +54,74 @@ file_put_contents($file, $count);
         </style>
             <title>Dashboard</title>
 
+            <head>
+        <title>::Login Page::</title>
+        <style type="text/css">
+            body{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background-size: cover;
+                background-image: url("https://cdn.arstechnica.net/wp-content/uploads/2023/06/bliss-update-1440x960.jpg");
+            }
+            table{
+                background-color: white;
+                border: 3px solid grey;
+                padding: 20px;
+                border-radius: 10px;
+                font-family:Arial, Helvetica, sans-serif;
+            }
+            td{
+                padding: 5px;
+            }
+            button{
+                background-color: greenyellow;
+                padding: 10px;
+                border-radius: 5px;
+            }
+            #logout {
+                background-color:rgb(236, 80, 80);
+                cursor: pointer;
+            }
+        </style>
     </head>
     <body>
         <h1><?php echo "Selamat datang " . $username . " ke-" . $count  ; ?></h1>
-
+            <form action="dashboard.php" method="post">
+         <table>
+            <tr>
+                <td colspan="2" style="text-align: center;" >DAFTAR</td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td><input type="text" name="nama" /></td>
+            </tr>
+            <tr>
+                <td>Umur</td>
+                <td><input type="text" name="umur" /></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center;">
+                    <button type="submit" >SUBMIT</button>
+                    <a href="logout.php">
+                        <button id="logout" type="button" >LOGOUT</button>
+                    </a>
+                </td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>Umur</td>
+            </tr>
+                <?php foreach($_SESSION["daftar"] as $daftar): ?>
+                 <tr>
+                    <td><?php echo $daftar["nama"] ?></td>
+                    <td><?php echo $daftar["umur"] ?></td>
+                 </tr>
+                 <?php endforeach; ?>
+        </table>
+        </form>
     </body>
 </html>
